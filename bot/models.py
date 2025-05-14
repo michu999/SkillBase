0e67265934794b0b9977ad3997ffe2a32d8e282c
michu999 <mgoluch2020@gmail.com>
2025-05-14 20:03:04 +0200

    added city form, improved model handling in the ORM

--- bot/models.py
+++ bot/models.py
@@ -9,9 +9,17 @@ class Skill(models.Model):
 
 class User(models.Model):
     auth_user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
+    email = models.EmailField(blank=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(blank=True)
+    skills = models.ManyToManyField(Skill, blank=True)
+    cities = models.ManyToManyField('City', blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.auth_user.username}) - {self.email}"
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
